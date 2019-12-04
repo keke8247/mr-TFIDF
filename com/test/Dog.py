9acb4d768e1a2bde5e53e961063a4717b70309a6
wdk <wdk@erongdu.com>
2019-12-04 18:01:19 +0800

    格式化代码

--- com/test/Dog.py
+++ com/test/Dog.py
@@ -5,11 +5,9 @@ class Dog():
         """初始化属性 name&age"""
         self.name = name
         self.age = age
-
     def sit(self):
         """模拟小狗被命令时蹲下"""
         print(self.name.title()+" is now sitting")
-
     def roll_over(self):
         """模拟小狗被命令时翻滚"""
         print (self.name.title()+ " is roll over")
